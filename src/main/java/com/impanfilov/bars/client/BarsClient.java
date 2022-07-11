d8e87c3b70f34236e8e409d412a7bb64fe55274c
Impanfilov <impanfilov@yandex.ru>
2022-07-11 21:04:50 +0300

    removed unnecessary

--- src/main/java/com/impanfilov/bars/client/BarsClient.java
+++ src/main/java/com/impanfilov/bars/client/BarsClient.java
@@ -5,15 +5,16 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import java.util.List;
 
+import java.util.List;
 
 @FeignClient(name = "bars", url = "${bars.url}",
         fallback = BarsClientFallback.class)
 
 public interface BarsClient {
-    @RequestMapping(method = RequestMethod.GET,value = "/getall")
+    @RequestMapping(method = RequestMethod.GET, value = "/getall")
     ResponseEntity<List<Contract>> getAll();
+
 
 }
 
--- src/main/java/com/impanfilov/bars/service/ContactServiceImpl.java
+++ src/main/java/com/impanfilov/bars/service/ContactServiceImpl.java
@@ -1,10 +1,12 @@
 package com.impanfilov.bars.service;
 
+
 import com.impanfilov.bars.client.BarsClient;
 import com.impanfilov.bars.model.Contract;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+
 
 
 @Service
@@ -17,4 +19,5 @@ public class ContactServiceImpl implements ContractService {
     public List<Contract> findAll() {
         return barsClient.getAll().getBody();
     }
+
 }
